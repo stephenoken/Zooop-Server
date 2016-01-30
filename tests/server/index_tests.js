481bcf51531db18f2b7bb1c0c0c610f1948a43c9
Stephen O'Kennedy <stephenoken@gmail.com>
2016-01-30 17:49:44 +0000

    "Added in a sample index page"
    ""

--- tests/server/index_tests.js
+++ tests/server/index_tests.js
@@ -9,16 +9,16 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 //Run an instance of the server
-var server = require("./../../server");
+const server = require("./../../server");
 
-describe('Sample Tests', function () {
+describe('Sample Tests',()=> {
   it('perform basic maths',()=>{
     expect(2 + 2).to.be.equal(4);
   });
 });
 
-describe('Index Routes', function () {
-  it('serve index.html', function (done) {
+describe('Index Routes',() => {
+  it('serve index.html', (done) =>{
     chai.request(server)
       .get('/')
       .end((err,res)=>{
