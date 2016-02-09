74b0d1d964c8fb0bd96d3eb27f746a6bb66b3b5d
anujsinghdm <singha2@tcd.ie>
2016-02-09 16:51:55 +0000

    Added function in Schema

--- backend_src/db/schema.js
+++ backend_src/db/schema.js
@@ -2,33 +2,18 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
-var Schema =  new mongoose.Schema(
-{
-	username:String,
-	Password: String	
-}
-);
 
-//insert document
-var user = mongoose.model('user', Schema);
-
-
-
-var u = new user({
-	username:"Anuj",
-	password:"pwrd"
-});
-
-
-u.save(function(err){
-	if(err){
-		console.log("We fucked up");
-	}
 	
-	console.log("saved");
-});
-
-//querying document
-user.findOne({username: "Stephen"}, function(err, document) {
-  console.log(document);
-});
+function insertCred(userId, passw) {
+	var Schema =  new mongoose.Schema({ username:String, Password: String});	
+	//insert document
+	var user = mongoose.model('user', Schema);
+	var u = new user({ 	username:userId, 	password:passw });  
+	u.save(function(err){
+		if(err){
+			console.log("We fucked up");
+		}	
+		console.log("saved");
+	});
+}
+insertCred("alex1", "mypass");
