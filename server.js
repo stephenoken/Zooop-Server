0f43044ef3ef12d3e42839b32466262f0b569fd7
Stephen O'Kennedy <stephenoken@gmail.com>
2016-02-16 16:52:17 +0000

    "Changed the view engine"
    ""

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.set('port', (process.env.PORT || 5001));
 
 // View engine setup
 app.set('views', path.join(__dirname,'app/views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
