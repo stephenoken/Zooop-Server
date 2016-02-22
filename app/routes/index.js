a30306a205e0ce804e35ed2ee5b97f875730e23a
Stephen O'Kennedy <stephenoken@gmail.com>
2016-02-22 11:25:14 +0000

    "Refactored the index routes file"
    ""

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,20 +7,14 @@ const accountCtrl = require('./../controllers/account');
 const router = express.Router();
 
 // router.get('/',mainCtrl.showPage('index',{title:'Zooop'}));
-router.get('/',(req,res)=>{
-  res.render('index',{title:"Zooop",currentUser:req.user});
-});
+router.get('/',mainCtrl.showPage('index',{title:"Zooop"}));
 
-router.get("/login",(req,res)=>{
-  res.render('login');
-});
+router.get("/login",mainCtrl.showPage('login'));
 
 router.post('/login', authCtrl.signin);
 router.post('/signup',accountCtrl.signup);
 router.get('/logout', authCtrl.signout);
 
-router.get("/dashboard",auth.ensured,(req,res)=>{
-  res.render('dashboard',{user:req.user});
-});
+router.get("/dashboard",auth.ensured,mainCtrl.showPage('dashboard',{test:"Hello World"}));
 
 module.exports = router;
