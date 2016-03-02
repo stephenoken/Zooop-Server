434af06ad107620e5912a1faf6aa682eafba05ea
Stephen O'Kennedy <stephenoken@gmail.com>
2016-03-02 11:44:49 +0000

    "Fixed browserify issues"
    ""

--- gulp_tasks/browserify.js
+++ gulp_tasks/browserify.js
@@ -35,5 +35,15 @@ function watch() {
   return compile(true);
 }
 
+gulp.task('prod-build-js',()=>{
+  var bundler = browserify('./public/scripts/app.js', { debug: true }).transform(babel);
+  return bundler.bundle()
+    .on('error', function(err) { console.error(err); this.emit('end'); })
+    .pipe(source('build.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/scripts'));
+});
 gulp.task('build-js', function() { return compile(); });
 gulp.task('watch-js', function() { return watch(); });
