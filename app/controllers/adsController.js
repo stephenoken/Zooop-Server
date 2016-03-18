6a081db4508227359c9468a2c990229ee0fdce65
Harpreet Singh <singhh@tcd.ie>
2016-03-18 16:28:02 +0000

    go stephen

--- app/controllers/adsController.js
+++ app/controllers/adsController.js
@@ -12,7 +12,9 @@ function sendAd () {
 function saveDiscoverAd () {
     return (req, res)=>{
         console.log(req.body);
-        var data = req.body;
+        // var data = req.body;
+        var data = Object.assign(req.body,{retailerId:req.user._id});
+    	console.log(data);
     	//var product = new Advertisment(req.body);
     	// Save the product and check for errors
     	Advertisment.create(data,function(err, advertisment){
@@ -28,12 +30,11 @@ function saveDiscoverAd () {
 
 function getDiscoverAds () {
 	return (req, res)=> {
-        Advertisment.find(function(err, adverts) {
+        Advertisment.find({ 'retailerId': req.user._id}, function(err, adverts) {
         	if (err)
         		res.send(err);
 
 						var discoverAds = adverts.map((advert)=>{
-						// 	 Retailer.findOne({_id:advert.retailerId},(err,retailer)=>{
 								return {
 									adInfo: advert,
 									shopInfo:{
