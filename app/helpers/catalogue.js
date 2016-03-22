4aa1ed268be57c7b31cbcdbc40afb69af3b3bbaf
anujsinghdm <singha2@tcd.ie>
2016-03-22 18:03:26 +0000

    suicide
    upload catalogue for manual inserttion of product

--- app/helpers/catalogue.js
+++ app/helpers/catalogue.js
@@ -2,4 +2,8 @@ function parseFile(file) {
 
 }
 
+function uploadByName(name) {
+ console.log(name);
+}
+
 module.exports.parseFile = parseFile;
--- app/helpers/master_product_helper.js
+++ app/helpers/master_product_helper.js
@@ -1,12 +1,36 @@
 'use strict';
 const Product = require("./../models/master_product");
-function searchMasterProducts(productName,res){
-  console.log(Product.findByName(productName,(product)=>{
-    console.log(product);
-    res.send(product);
-  }));
-  console.log("2");
+const retailerProduct = require("./../models/retailer_product");
+var mongoose = require('mongoose');
+function searchMasterProducts(object){
+  Product.findOne({name:object.name},function(err, doc) {
+        saveDiscoverProduct(doc, object.retailerId)
+  });
 }
 
+function saveDiscoverProduct(object, retailerId) {
+    var name = object.name;
+    var category = object.category;
+    var taste = object.properties.taste;
+    var origin = object.properties.origin;
+    var dietaryTags = object.properties.dietaryTags;
+    var newProd = mongoose.model('DiscoverProduct', retailerProduct);
+    var product = new newProd({
+                                retailerId: retailerId,
+                                name: name,
+                                properties: {
+                                  taste:taste,
+                                  dietaryTags:dietaryTags,
+                                  category: category
+                                }
+                          });
+
+    product.save(function(err){
+              		if(err){
+              			console.log(err);
+              		}
+              		console.log("Product added");
+              	});
+}
 
 module.exports.searchMasterProducts = searchMasterProducts;
