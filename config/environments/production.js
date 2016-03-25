bbb20f122825e71b85250d0c48128e73f62c0f88
Stephen O'Kennedy <stephenoken@gmail.com>
2016-03-25 15:06:30 +0000

    "Add GCM helper method"
    ""

--- config/environments/production.js
+++ config/environments/production.js
@@ -4,5 +4,6 @@ module.exports = {
   app: "Zooop",
   mongodb: {
     uri: 'mongodb://heroku_jh0bnx04:oiq7cgs384pumjt10b290tm0fi@ds039010.mongolab.com:39010/heroku_jh0bnx04'
-  }
+  },
+  googleAPIKey:'AIzaSyAE36hHZJEtECBiyhiifulSt2JRQ6elLlw'
 };
--- config/environments/test.js
+++ config/environments/test.js
@@ -4,5 +4,6 @@ module.exports = {
   app: "Zooop",
   mongodb: {
     uri: 'mongodb://localhost/zooop-test'
-  }
+  },
+  googleAPIKey:'AIzaSyAE36hHZJEtECBiyhiifulSt2JRQ6elLlw'
 };
