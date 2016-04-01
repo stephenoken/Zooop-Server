7579b7d6aef6f8a040b24ffa3155aa1947f19ff6
Stephen O'Kennedy <stephenoken@gmail.com>
2016-04-01 15:55:12 +0100

    "Fixed failling tests"
    ""

--- tests/server/fixtures/advertisements.js
+++ tests/server/fixtures/advertisements.js
@@ -5,17 +5,20 @@ module.exports = [{
 	"name" : "Advert1",
 	"description" : "test",
 	"retailerId" : "554b6fe1a42e7bbc1e932344",
-  "tags": ["Retailer 1"]
+  "tags": ["Retailer 1"],
+  "imgUrl": 'https://www.google.ie/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
 },{
   "type" : "Test",
 	"name" : "Advert2",
 	"description" : "test",
 	"retailerId" : "554b6fe1a42e7bbc1e932344",
-  "tags": ["Retailer 1"]
+  "tags": ["Retailer 1"],
+  "imgUrl": 'https://www.google.ie/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
 },{
   "type" : "Test",
 	"name" : "Advert3",
 	"description" : "test",
 	"retailerId" : "554b6fe1a42e7bbc1e932345",
-  "tags": ["Retailer 2"]
+  "tags": ["Retailer 2"],
+  "imgUrl": 'https://www.google.ie/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
 }];
